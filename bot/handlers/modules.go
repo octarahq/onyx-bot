ac169ed29f41ebd5381ac61ff5437ddac93d3189
lotus64 <lotus64santon@gmail.com>
2026-07-06 18:53:31 +0200

    refactor(modules): module handler

--- bot/handlers/modules.go
+++ bot/handlers/modules.go
@@ -4,7 +4,9 @@ import (
 	"onyx/bot/core"
 
 	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/snowflake/v2"
 )
 
 func ExecModulesEvent(b *core.Bot, event bot.Event) bool {
@@ -19,27 +21,40 @@ func ExecModulesEvent(b *core.Bot, event bot.Event) bool {
 		return false
 	}
 
-	var guildIDStr string
+	var guildID *snowflake.ID
 
 	switch e := event.(type) {
 	case *events.MessageCreate:
-		if e.GuildID != nil {
-			guildIDStr = e.GuildID.String()
-		}
+		guildID = e.GuildID
 	}
 
-	if guildIDStr == "" {
+	if guildID == nil {
 		return false
 	}
 
+	guildIDStr := guildID.String()
+
+	me, ok := event.Client().Caches.Member(*guildID, event.Client().ID())
+	if !ok {
+		m, err := event.Client().Rest.GetMember(*guildID, event.Client().ID())
+		if err != nil {
+			return false
+		}
+		me = *m
+	}
+
 	for _, mod := range b.Modules {
+		if !mod.IsEnabled() {
+			continue
+		}
+
 		if dbAware, ok := mod.(core.DatabaseAware); ok {
 			if err := dbAware.LoadData(b.DB.GormDB, guildIDStr); err != nil {
 				continue
 			}
 		}
 
-		if !mod.IsEnabled() {
+		if !CheckPerms(b, mod, me) {
 			continue
 		}
 
@@ -53,5 +68,26 @@ func ExecModulesEvent(b *core.Bot, event bot.Event) bool {
 		}
 	}
 
+	return false
+}
+
+func CheckPerms(b *core.Bot, module core.Module, me discord.Member) bool {
+	var missing []string
+	botPerms := b.Client.Caches.MemberPermissions(me)
+
+	if botPerms.Has(discord.PermissionAdministrator) {
+		return true
+	}
+
+	permissions := module.Permissions()
+	for _, p := range permissions {
+		if !botPerms.Has(p) {
+			missing = append(missing, p.String())
+		}
+	}
+
+	if len(missing) == 0 {
+		return true
+	}
 	return false
 }
