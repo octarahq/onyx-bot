1add55cd98af9b3bb666828c0f732d3ea1aebb33
lotus64 <lotus64santon@gmail.com>
2026-07-15 17:00:43 +0200

    feat(modules): add base WelcomeModule

---
+++ bot/modules/WelcomeModule.go
@@ -0,0 +1,278 @@
+package modules
+
+import (
+	"onyx/bot/core"
+	"onyx/bot/locales"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+	"gorm.io/gorm"
+)
+
+type WelcomeJoinSettings struct {
+	Enabled       bool   `gorm:"default:false" json:"enabled"`
+	Channel       string `json:"channel"`
+	Content       string `json:"content"`
+	MentionMember bool   `gorm:"default:true" json:"mentionMember"`
+}
+
+type WelcomeLeaveSettings struct {
+	Enabled       bool   `gorm:"default:false" json:"enabled"`
+	Channel       string `json:"channel"`
+	Content       string `json:"content"`
+	MentionMember bool   `gorm:"default:true" json:"mentionMember"`
+}
+
+type WelcomeSettings struct {
+	GuildID string               `gorm:"primaryKey" json:"guild_id"`
+	Enabled bool                 `gorm:"default:false" json:"enabled"`
+	Join    WelcomeJoinSettings  `gorm:"embedded;embeddedPrefix:join_" json:"join"`
+	Leave   WelcomeLeaveSettings `gorm:"embedded;embeddedPrefix:leave_" json:"leave"`
+}
+
+type WelcomeModule struct {
+	Data WelcomeSettings
+}
+
+func init() {
+	Register(&WelcomeModule{})
+}
+
+func (m *WelcomeModule) HandleGuildMemberJoin(b *core.Bot, e *events.GuildMemberJoin) bool {
+	if m.Data.Enabled && m.Data.Join.Enabled {
+	}
+	return false
+}
+
+func (m *WelcomeModule) HandleGuildMemberLeave(b *core.Bot, e *events.GuildMemberLeave) bool {
+	if m.Data.Enabled && m.Data.Leave.Enabled {
+	}
+	return false
+}
+
+func (m *WelcomeModule) Metadata() core.Metadata {
+	return core.Metadata{
+		Name: "WelcomeModule",
+		Icon: "waving_hand",
+		Label: func(locale discord.Locale) string {
+			return locales.GetMeta(locale, "module_WelcomeModule").Label
+		},
+		Description: func(locale discord.Locale) string {
+			return locales.GetMeta(locale, "module_WelcomeModule").Description
+		},
+		Submodules: func(locale discord.Locale) map[string]core.SubmoduleMeta {
+			meta := locales.GetMeta(locale, "module_WelcomeModule")
+			subs := make(map[string]core.SubmoduleMeta)
+			for k, v := range meta.Submodules {
+				subs[k] = core.SubmoduleMeta{
+					Label:       v.Label,
+					Description: v.Description,
+				}
+			}
+			return subs
+		},
+	}
+}
+
+func (m *WelcomeModule) Priority() int   { return 1 }
+func (m *WelcomeModule) IsEnabled() bool { return m.Data.Enabled }
+func (m *WelcomeModule) Permissions() []discord.Permissions {
+	return []discord.Permissions{
+		discord.PermissionSendMessages,
+	}
+}
+
+func (m *WelcomeModule) Schema() interface{}  { return &WelcomeSettings{} }
+func (m *WelcomeModule) DataPtr() interface{} { return &m.Data }
+func (m *WelcomeModule) LoadData(db *gorm.DB, guildID string) error {
+	m.Data = WelcomeSettings{GuildID: guildID}
+	return db.FirstOrCreate(&m.Data, WelcomeSettings{GuildID: guildID}).Error
+}
+
+func (m *WelcomeModule) UISchema(locale discord.Locale) core.UISchema {
+	meta := locales.GetMeta(locale, "module_WelcomeModule")
+
+	joinLabel := "Join Settings"
+	joinDesc := ""
+	joinEnabledLabel := "Enabled"
+	joinChannelLabel := "Channel"
+	joinChannelDesc := "The channel where the message will be sent."
+	joinContentLabel := "Message Content"
+	joinMentionLabel := "Mention Member"
+
+	joinVars := []core.Variables{
+		{Key: "user.id", Label: "User ID", Description: "The ID of the user."},
+		{Key: "user.mention", Label: "User Mention", Description: "Mentions the user."},
+		{Key: "user.name", Label: "User Name", Description: "The username of the user."},
+		{Key: "server.name", Label: "Server Name", Description: "The name of the server."},
+		{Key: "server.member_count", Label: "Member Count", Description: "The number of members in the server."},
+	}
+
+	if sub, ok := meta.Submodules["join"]; ok {
+		if sub.Label != "" {
+			joinLabel = sub.Label
+		}
+		if sub.Description != "" {
+			joinDesc = sub.Description
+		}
+		if opt, ok := sub.Options["enabled"]; ok && opt.Label != "" {
+			joinEnabledLabel = opt.Label
+		}
+		if opt, ok := sub.Options["channel"]; ok {
+			if opt.Label != "" {
+				joinChannelLabel = opt.Label
+			}
+			if opt.Description != "" {
+				joinChannelDesc = opt.Description
+			}
+		}
+		if opt, ok := sub.Options["content"]; ok {
+			if opt.Label != "" {
+				joinContentLabel = opt.Label
+			}
+			for i, v := range joinVars {
+				if vOpt, ok := opt.Options[v.Key]; ok {
+					if vOpt.Label != "" {
+						joinVars[i].Label = vOpt.Label
+					}
+					if vOpt.Description != "" {
+						joinVars[i].Description = vOpt.Description
+					}
+				}
+			}
+		}
+		if opt, ok := sub.Options["mentionMember"]; ok && opt.Label != "" {
+			joinMentionLabel = opt.Label
+		}
+	}
+
+	leaveLabel := "Leave Settings"
+	leaveDesc := ""
+	leaveEnabledLabel := "Enabled"
+	leaveChannelLabel := "Channel"
+	leaveChannelDesc := "The channel where the message will be sent."
+	leaveContentLabel := "Message Content"
+	leaveMentionLabel := "Mention Member"
+
+	leaveVars := []core.Variables{
+		{Key: "user.id", Label: "User ID", Description: "The ID of the user."},
+		{Key: "user.name", Label: "User Name", Description: "The username of the user."},
+		{Key: "server.name", Label: "Server Name", Description: "The name of the server."},
+		{Key: "server.member_count", Label: "Member Count", Description: "The number of members in the server."},
+	}
+
+	if sub, ok := meta.Submodules["leave"]; ok {
+		if sub.Label != "" {
+			leaveLabel = sub.Label
+		}
+		if sub.Description != "" {
+			leaveDesc = sub.Description
+		}
+		if opt, ok := sub.Options["enabled"]; ok && opt.Label != "" {
+			leaveEnabledLabel = opt.Label
+		}
+		if opt, ok := sub.Options["channel"]; ok {
+			if opt.Label != "" {
+				leaveChannelLabel = opt.Label
+			}
+			if opt.Description != "" {
+				leaveChannelDesc = opt.Description
+			}
+		}
+		if opt, ok := sub.Options["content"]; ok {
+			if opt.Label != "" {
+				leaveContentLabel = opt.Label
+			}
+			for i, v := range leaveVars {
+				if vOpt, ok := opt.Options[v.Key]; ok {
+					if vOpt.Label != "" {
+						leaveVars[i].Label = vOpt.Label
+					}
+					if vOpt.Description != "" {
+						leaveVars[i].Description = vOpt.Description
+					}
+				}
+			}
+		}
+		if opt, ok := sub.Options["mentionMember"]; ok && opt.Label != "" {
+			leaveMentionLabel = opt.Label
+		}
+	}
+
+	maxContent := 1000
+
+	return core.UISchema{
+		SubModules: []core.UISubModule{
+			{
+				Name:        "join",
+				Label:       joinLabel,
+				Description: joinDesc,
+				Components: []core.UIComponent{
+					{
+						Name:     "enabled",
+						Label:    joinEnabledLabel,
+						Type:     core.ComponentTypeBoolean,
+						Required: false,
+					},
+					{
+						Name:         "channel",
+						Label:        joinChannelLabel,
+						Description:  joinChannelDesc,
+						Type:         core.ComponentTypeChannel,
+						Required:     false,
+						ChannelTypes: []discord.ChannelType{discord.ChannelTypeGuildText},
+					},
+					{
+						Name:      "content",
+						Label:     joinContentLabel,
+						Type:      core.ComponentTypeTextarea,
+						Max:       &maxContent,
+						Required:  false,
+						Variables: joinVars,
+					},
+					{
+						Name:     "mentionMember",
+						Label:    joinMentionLabel,
+						Type:     core.ComponentTypeBoolean,
+						Required: false,
+					},
+				},
+			},
+			{
+				Name:        "leave",
+				Label:       leaveLabel,
+				Description: leaveDesc,
+				Components: []core.UIComponent{
+					{
+						Name:     "enabled",
+						Label:    leaveEnabledLabel,
+						Type:     core.ComponentTypeBoolean,
+						Required: false,
+					},
+					{
+						Name:         "channel",
+						Label:        leaveChannelLabel,
+						Description:  leaveChannelDesc,
+						Type:         core.ComponentTypeChannel,
+						Required:     false,
+						ChannelTypes: []discord.ChannelType{discord.ChannelTypeGuildText},
+					},
+					{
+						Name:      "content",
+						Label:     leaveContentLabel,
+						Type:      core.ComponentTypeTextarea,
+						Max:       &maxContent,
+						Required:  false,
+						Variables: leaveVars,
+					},
+					{
+						Name:     "mentionMember",
+						Label:    leaveMentionLabel,
+						Type:     core.ComponentTypeBoolean,
+						Required: false,
+					},
+				},
+			},
+		},
+	}
+}
