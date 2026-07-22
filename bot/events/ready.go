25bed8594f0954eda8e527b1b56d2b3561fcd9dc
lotus64 <lotus64santon@gmail.com>
2026-07-22 09:45:19 +0200

    feat: add bot presence

--- bot/events/ready.go
+++ bot/events/ready.go
@@ -1,13 +1,15 @@
 package events
 
 import (
+	"context"
 	"fmt"
 	"onyx/bot/core"
 	"onyx/bot/handlers"
-
+	"time"
 
 	"github.com/disgoorg/disgo/bot"
 	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
 )
 
 func init() {
@@ -29,6 +31,33 @@ func init() {
 				fmt.Printf("Successfully synced %d commands!\n", len(b.Commands))
 				fmt.Printf("Successfully %d modules!\n", len(b.Modules))
 			}
+
+			go func() {
+				ticker := time.NewTicker(15 * time.Second)
+				defer ticker.Stop()
+
+				state := 0
+				for {
+					var status string
+					switch state {
+					case 0:
+						status = "onyx.octara.xyz"
+					case 1:
+						serverCount := b.Client.Caches.GuildsLen()
+						status = fmt.Sprintf("I'm in %d servers", serverCount)
+					case 2:
+						status = "Open-Source bot"
+					}
+
+					err := b.Client.SetPresence(context.Background(), gateway.WithCustomActivity(status))
+					if err != nil {
+						fmt.Printf("Failed to set presence: %v\n", err)
+					}
+
+					state = (state + 1) % 3
+					<-ticker.C
+				}
+			}()
 		},
 	})
 }
