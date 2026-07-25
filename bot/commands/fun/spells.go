938ffbf401bbd9565d788488066e76a554325247
lotus64 <lotus64santon@gmail.com>
2026-07-25 18:19:15 +0200

    feat(cmd): add /spells

---
+++ bot/commands/fun/spells.go
@@ -0,0 +1,83 @@
+package fun
+
+import (
+	"fmt"
+	"strings"
+
+	"onyx/bot/core"
+	"onyx/bot/handlers"
+	"onyx/bot/locales"
+	"onyx/bot/modules"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+)
+
+func init() {
+	handlers.RegisterCommand(handlers.Command{
+		Name:        "spells",
+		Description: "Get spells informations",
+		Category:    "Utils",
+		Create: discord.SlashCommandCreate{
+			Name:        "spells",
+			Description: "Get spells information",
+			Options: []discord.ApplicationCommandOption{
+				discord.ApplicationCommandOptionString{
+					Name:         "spell",
+					Description:  "The spell",
+					Required:     true,
+					Autocomplete: true,
+				},
+			},
+		},
+		ExecuteAutocomplete: func(b *core.Bot, event *events.AutocompleteInteractionCreate) {
+			searchOpt, _ := event.Data.Option("spell")
+			search := strings.ToLower(fmt.Sprint(searchOpt))
+			spells := modules.InitWizzardSpells(locales.GetModule_WizzardModule(event.Locale()))
+			choices := make([]discord.AutocompleteChoice, 0, 25)
+
+			for _, s := range spells {
+				name := strings.ToLower(s.Name)
+				if search != "" && !strings.Contains(name, search) {
+					continue
+				}
+
+				choices = append(choices, discord.AutocompleteChoiceString{
+					Name:  s.Name,
+					Value: s.Key,
+				})
+				if len(choices) == 25 {
+					break
+				}
+			}
+
+			_ = event.Respond(discord.InteractionResponseTypeAutocompleteResult, discord.AutocompleteResult{
+				Choices: choices,
+			})
+		},
+		Execute: func(b *core.Bot, event *events.ApplicationCommandInteractionCreate) {
+			cmd := event.SlashCommandInteractionData()
+			s, _ := cmd.Option("spell")
+			spells := modules.InitWizzardSpells(locales.GetModule_WizzardModule(event.Locale()))
+
+			var spell modules.Sort
+			for _, sort := range spells {
+				if sort.Key == s.String() {
+					spell = sort
+					break
+				}
+			}
+
+			msg := discord.NewMessageCreateV2(
+				discord.NewContainer(
+					discord.NewTextDisplay("# " + spell.Name),
+					discord.NewTextDisplay("> *" + spell.Description + "*"),
+				),
+			)
+
+			if err := event.CreateMessage(msg); err != nil {
+
+			}
+		},
+	})
+}
